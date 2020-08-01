4044339d11a95178fd28ebe4dc890059b1a0b1dd
Wesley Shann <sshann95@outlook.com>
2020-08-01 15:44:20 +0200

    Log F1 score metric after every update

--- Prefetching-Library/android_prefetching_lib/src/main/java/nl/vu/cs/s2group/nappa/Nappa.java
+++ Prefetching-Library/android_prefetching_lib/src/main/java/nl/vu/cs/s2group/nappa/Nappa.java
@@ -36,6 +36,7 @@ import nl.vu.cs.s2group.nappa.graph.ActivityNode;
 import nl.vu.cs.s2group.nappa.handler.activity.RegisterNewActivityHandler;
 import nl.vu.cs.s2group.nappa.handler.graph.InitGraphHandler;
 import nl.vu.cs.s2group.nappa.handler.session.RegisterNewSessionHandler;
+import nl.vu.cs.s2group.nappa.nappaexperimentation.MetricPrefetchingAccuracy;
 import nl.vu.cs.s2group.nappa.nappaexperimentation.MetricStrategyAccuracy;
 import nl.vu.cs.s2group.nappa.prefetch.PrefetchingStrategy;
 import nl.vu.cs.s2group.nappa.prefetch.PrefetchingStrategyConfigKeys;
@@ -657,6 +658,19 @@ public class Nappa {
                 // If is a GET request, is not a prefetch itself and was not previously prefetched --> add to the FN list
                 add_fn_url(url);
         }
+        Log.d(LOG_TAG, "-------------F1_SCORE #" + Nappa.metricPrefetchingAccuracyID + " -----------");
+        Log.d(LOG_TAG, "F1_SCORE - " + "Intercept list (" + Nappa.list_url_intercepted.size() + ")= " + Nappa.list_url_intercepted.toString());
+        Log.d(LOG_TAG, "F1_SCORE - " + "Prefetch list (" + Nappa.list_url_prefetched.size() + ")= " + Nappa.list_url_prefetched.toString());
+        Log.d(LOG_TAG, "F1_SCORE - " + "TP list (" + Nappa.list_url_tp.size() + ")= " + Nappa.list_url_tp.toString());
+        Log.d(LOG_TAG, "F1_SCORE - " + "FN list (" + Nappa.list_url_fn.size() + ")= " + Nappa.list_url_fn.toString());
+        Nappa.metricPrefetchingAccuracyID++;
+        int truePositive = Nappa.list_url_tp.size();
+        int falseNegative = Nappa.list_url_fn.size();
+        int falsePositive = 0;
+        for (String prefetchedUrls : Nappa.list_url_prefetched) {
+            if (!Nappa.list_url_intercepted.contains(prefetchedUrls)) falsePositive++;
+        }
+        MetricPrefetchingAccuracy.log(Nappa.metricPrefetchingAccuracyID, truePositive, falsePositive, falseNegative);
     }
     /* Thesis experimentation for getting F1 Score - End */
 
