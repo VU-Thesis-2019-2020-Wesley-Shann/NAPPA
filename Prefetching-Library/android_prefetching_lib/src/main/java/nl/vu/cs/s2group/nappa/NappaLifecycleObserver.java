fbe69e988bcfe78b05977529ba83a5d30df62fe3
Wesley Shann <sshann95@outlook.com>
2020-08-02 15:17:47 +0200

    Log prefetch accuracy on destroy

--- Prefetching-Library/android_prefetching_lib/src/main/java/nl/vu/cs/s2group/nappa/NappaLifecycleObserver.java
+++ Prefetching-Library/android_prefetching_lib/src/main/java/nl/vu/cs/s2group/nappa/NappaLifecycleObserver.java
@@ -7,6 +7,8 @@ import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.LifecycleObserver;
 import androidx.lifecycle.OnLifecycleEvent;
 
+import nl.vu.cs.s2group.nappa.nappaexperimentation.MetricPrefetchingAccuracy;
+
 public class NappaLifecycleObserver implements LifecycleObserver {
     private static final String LOG_TAG = NappaLifecycleObserver.class.getSimpleName();
 
@@ -28,8 +30,25 @@ public class NappaLifecycleObserver implements LifecycleObserver {
         Nappa.leavingCurrentActivity();
     }
 
-//    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-//    public void onDestroy() {
-//        Log.d(LOG_TAG, activity.getClass().getCanonicalName() + " - onDestroy");
-//    }
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    public void onDestroy() {
+        Log.d(LOG_TAG, activity.getClass().getCanonicalName() + " - onDestroy");
+        logPrefetchAccuracy();
+    }
+
+    private void logPrefetchAccuracy(){
+        Log.d(LOG_TAG, "-------------F1_SCORE #" + Nappa.metricPrefetchingAccuracyID + " -----------");
+        Log.d(LOG_TAG, "F1_SCORE - " + "Intercept list (" + Nappa.list_url_intercepted.size() + ")= " + Nappa.list_url_intercepted.toString());
+        Log.d(LOG_TAG, "F1_SCORE - " + "Prefetch list (" + Nappa.list_url_prefetched.size() + ")= " + Nappa.list_url_prefetched.toString());
+        Log.d(LOG_TAG, "F1_SCORE - " + "TP list (" + Nappa.list_url_tp.size() + ")= " + Nappa.list_url_tp.toString());
+        Log.d(LOG_TAG, "F1_SCORE - " + "FN list (" + Nappa.list_url_fn.size() + ")= " + Nappa.list_url_fn.toString());
+        Nappa.metricPrefetchingAccuracyID++;
+        int truePositive = Nappa.list_url_tp.size();
+        int falseNegative = Nappa.list_url_fn.size();
+        int falsePositive = 0;
+        for (String prefetchedUrls : Nappa.list_url_prefetched) {
+            if (!Nappa.list_url_intercepted.contains(prefetchedUrls)) falsePositive++;
+        }
+        MetricPrefetchingAccuracy.log(Nappa.metricPrefetchingAccuracyID, truePositive, falsePositive, falseNegative);
+    }
 }
